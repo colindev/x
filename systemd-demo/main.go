d34da5e2a496cdb95f238e32ab02114b9b3e5599
colin <colin1124x@gmail.com>
2016-10-20 11:14:18 +0800

    update

--- systemd-demo/main.go
+++ systemd-demo/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 )
 
 func main() {
@@ -22,10 +25,19 @@ func main() {
 	flag.StringVar(&addr, "addr", ":8000", "http serve on")
 	flag.Parse()
 
+	// 輸出 pid
 	if err := ioutil.WriteFile(pidfile, []byte(strconv.Itoa(os.Getpid())), os.ModePerm); err != nil {
 		log.Println(err)
 	}
 
+	// 處理 signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGQUIT)
+	s := <-c
+	os.Stdout.WriteString(fmt.Sprintf("%v(%#v)\n", s, s))
+}
+
+func runHTTP(addr string) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 		log.Println(r.URL)
@@ -34,5 +46,4 @@ func main() {
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Println(err)
 	}
-
 }
