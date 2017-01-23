05701b586c23e808ff625bc47a29ee62e32d687d
colin <colin1124x@gmail.com>
2017-01-23 12:31:29 +0800

    example

--- gapi/main.go
+++ gapi/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"golang.org/x/oauth2/jwt"
@@ -10,13 +13,13 @@ import (
 
 type Scopes []string
 
-func (s Scopes) Set(v string) error {
-	s = append(s, v)
+func (s *Scopes) Set(v string) error {
+	*s = append(*s, v)
 	return nil
 }
 
-func (s Scopes) String() string {
-	return fmt.Sprintf("%v", ([]string)(s))
+func (s *Scopes) String() string {
+	return fmt.Sprintf("%v", ([]string)(*s))
 }
 
 var (
@@ -37,7 +40,57 @@ func main() {
 
 	flag.Parse()
 
-	conf := &jwt.Config{
-		Scopes: ([]string)(scopes),
+	fmt.Println(scopes)
+
+	b, err := ioutil.ReadFile(secretFile)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	var ser Secret
+	if err := json.Unmarshal(b, &ser); err != nil {
+		log.Fatal(err)
+	}
+
+	conf := &jwt.Config{
+		Email:      ser.ClientEmail,
+		PrivateKey: []byte(ser.PrivateKey),
+		TokenURL:   ser.TokenURI,
+		Scopes:     ([]string)(scopes),
+	}
+
+	tok, err := conf.TokenSource(context.Background()).Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !tok.Valid() {
+		log.Fatal("valid fail")
+	}
+
+	c := conf.Client(context.Background())
+
+	res, err := c.Get("https://www.googleapis.com/bigquery/v2/projects/xxxx-155908/datasets?key=")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	b, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+}
+
+type Secret struct {
+	Type         string `json:"type"`
+	ProjectID    string `json:"project_id"`
+	PrivateKeyID string `json:"project_key_id"`
+	PrivateKey   string `json:"private_key"`
+	ClientEmail  string `json:"client_email"`
+	ClientID     string `json:"client_id"`
+	AuthURI      string `json:"auth_uri"`
+	TokenURI     string `json:"token_uri"`
 }
